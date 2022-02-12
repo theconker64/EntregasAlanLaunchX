8478e4b9b1500ff88bebe64230357d68288dfdbf
theconker64 <73414537+theconker64@users.noreply.github.com>
2022-02-11 20:34:52 -0700

    Katas Entregas

---
+++ Modulo 1 - Kata (convertidor de unidades).py
@@ -0,0 +1,6 @@
+## Modaulo 1- Kata (convertidor de unidades)
+
+parsec = 3.26156
+par = int(input("Escribe la cantidad de Parsec a convertir: "))
+lightyears = (par * parsec)
+print(str(par) + " parsec convertidos a años luz es igual a: " + str(lightyears))
